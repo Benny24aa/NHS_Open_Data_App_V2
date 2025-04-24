2a0a8666356596aa4bd14aa06f64f430f5f74db3
Benny24aa <harleyb101010@gmail.com>
2025-04-24 18:34:10 +0100

    Added tooltips to sex breakdowns, and also added rounding

--- Open Data Dashboard/Server/Overview Graphs.R
+++ Open Data Dashboard/Server/Overview Graphs.R
@@ -3,13 +3,19 @@ output$scotland_info_graph_server <- renderPlotly({
     select(-CancerSiteICD10Code) %>% 
       filter(CancerSite == input$Cancer_Type_Input) %>% 
     filter(Sex == "All") %>% 
-    select(Year, AllAges, CrudeRate, EASR, WASR, StandardisedRatio, HBName, DataType) %>% 
+    select(Year, AllAges, CrudeRate, EASR, WASR, StandardisedRatio, HBName, DataType) %>%
     filter(HBName %in% input$hb_name) %>% select(-HBName) %>% 
-    filter(DataType == input$datatype_input) %>% 
+    filter(DataType == input$datatype_input)
+    
+    tooltip_1 <- c(paste0("Health Board: ", input$hb_name,  "<br>", "Data Type: ", input$datatype_input, "<br>",  "Measure Type: ", input$graphtype_input, "<br>", "Date: ", Cancer_Full_Data$Year, "<br>", "Total (All Ages): ", Cancer_Full_Data$AllAges,  "<br>",  "Crude Rate: ", round(Cancer_Full_Data$CrudeRate,2 ),  "<br>", "EASR: ", round(Cancer_Full_Data$EASR,2 ),  "<br>", "WASR: ", round(Cancer_Full_Data$WASR,2),  "<br>", "Standardised Ratio: ", round(Cancer_Full_Data$StandardisedRatio,2)))
+    
+    Cancer_Full_Data <- Cancer_Full_Data %>% 
       plot_ly(x = ~ Year,
               y = ~ get(input$graphtype_input),
               type = 'scatter',
-              mode = 'lines') %>% 
+              mode = 'lines',
+              text=tooltip_1, 
+              hoverinfo="text") %>% 
       layout(xaxis = list(title = "Year"),
              yaxis = list(title = input$graphtype_input))
    
@@ -22,12 +28,19 @@ output$scotland_gender_graph_server <- renderPlotly({
     filter(Sex != "All") %>% 
     select(Year, Sex, AllAges, CrudeRate, EASR, WASR, StandardisedRatio, HBName, DataType) %>% 
     filter(HBName  %in% input$hb_name) %>% select(-HBName) %>% 
-    filter(DataType == input$datatype_input) %>% 
+    filter(DataType == input$datatype_input)
+  
+  
+  tooltip_1 <- c(paste0("Health Board: ", input$hb_name,  "<br>", "Data Type: ", input$datatype_input, "<br>",  "Measure Type: ", input$graphtype_input, "<br>", "Date: ", Cancer_Full_Data$Year, "<br>", "Gender: ", Cancer_Full_Data$Sex, "<br>", "Total (All Ages): ", Cancer_Full_Data$AllAges,  "<br>",  "Crude Rate: ", round(Cancer_Full_Data$CrudeRate,2 ),  "<br>", "EASR: ", round(Cancer_Full_Data$EASR,2 ),  "<br>", "WASR: ", round(Cancer_Full_Data$WASR,2),  "<br>", "Standardised Ratio: ", round(Cancer_Full_Data$StandardisedRatio,2)))
+  
+  Cancer_Full_Data <- Cancer_Full_Data %>% 
     plot_ly(x = ~ Year,
             y = ~ get(input$graphtype_input),
             color = ~ Sex, colors = gender_palette,
             type = 'scatter',
-            mode = 'lines') %>% 
+            mode = 'lines',
+            text=tooltip_1, 
+            hoverinfo="text") %>% 
     layout(xaxis = list(title = "Year"),
            yaxis = list(title = input$graphtype_input))
   
