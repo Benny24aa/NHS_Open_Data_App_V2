82637a1ff852ed62fad1950a00b143c77c3df5f8
Benny24aa <harleyb101010@gmail.com>
2025-05-02 20:39:30 +0100

    added back graph compare for hb's after error

--- Open Data Dashboard/Server/Overview Graphs.R
+++ Open Data Dashboard/Server/Overview Graphs.R
@@ -46,6 +46,31 @@ output$scotland_gender_graph_server <- renderPlotly({
   
 })
 
+output$hb_compare_graph <- renderPlotly({
+  Cancer_Full_Data <- Cancer_Full_Data %>% 
+    select(-CancerSiteICD10Code) %>% 
+    filter(Sex == "All") %>% 
+    select(Year, AllAges, CrudeRate, EASR, WASR, StandardisedRatio, HBName, DataType, CancerSite) %>% 
+    filter(DataType == input$datatype_input_compare) %>% 
+    filter(HBName != "All Scotland Data") %>% 
+    filter(CancerSite == input$Cancer_Type_Input_compare) 
+  
+  tooltip_1 <- c(paste0("Health Board: ", Cancer_Full_Data$HBName,  "<br>", "Data Type: ", input$datatype_input_compare, "<br>",  "Measure Type: ", input$graphtype_input_compare, "<br>", "Date: ", Cancer_Full_Data$Year, "<br>", "Total (All Ages): ", Cancer_Full_Data$AllAges,  "<br>",  "Crude Rate: ", round(Cancer_Full_Data$CrudeRate,2 ),  "<br>", "EASR: ", round(Cancer_Full_Data$EASR,2 ),  "<br>", "WASR: ", round(Cancer_Full_Data$WASR,2),  "<br>", "Standardised Ratio: ", round(Cancer_Full_Data$StandardisedRatio,2)))
+  
+  
+  Cancer_Full_Data <- Cancer_Full_Data %>% 
+    plot_ly(x = ~ Year,
+            y = ~ get(input$graphtype_input_compare),
+            color = ~ HBName,
+            type = 'scatter',
+            mode = 'lines',
+            text=tooltip_1, 
+            hoverinfo="text" ) %>% 
+    layout(xaxis = list(title = "Year"),
+           yaxis = list(title = input$graphtype_input_compare))
+  
+})
+
 
 
 
@@ -76,11 +101,11 @@ output$hb_cancer_outlier_box <-  renderPlotly({
     filter(GeoName %in% input$hb_name) %>% 
     filter(Sex == input$Cancer_Gender_Input) %>% 
   plot_ly(x = ~GeoName,
-          y = ~DeathsAllAges,
+          y = ~get(input$BoxPlot_Input_Cancer),
           color = ~ GeoName,
           type = "box",
           quartilemethod="inclusive")%>% 
-    layout(xaxis = list(title = "All Ages"),
-           yaxis = list(title = "All Deaths"))
+    layout(xaxis = list(title = "Health Board Name"),
+           yaxis = list(title = input$BoxPlot_Input_Cancer))
   
 })
