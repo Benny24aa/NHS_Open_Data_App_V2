b6eb1a4d326927eeaad75753c4187de6e0c26a99
Benny24aa <harleyb101010@gmail.com>
2025-05-02 20:22:19 +0100

    Statistical Analytics graphs added such as plot box and scatterplot

--- Open Data Dashboard/Server/Overview Graphs.R
+++ Open Data Dashboard/Server/Overview Graphs.R
@@ -49,28 +49,38 @@ output$scotland_gender_graph_server <- renderPlotly({
 
 
 
-output$hb_compare_graph <- renderPlotly({
-  Cancer_Full_Data <- Cancer_Full_Data %>% 
+output$hb_cancer_outlier <- renderPlotly({
+  Cancer_Scatter_Data <-  Cancer_Scatter_Data %>% 
     select(-CancerSiteICD10Code) %>% 
-    filter(Sex == "All") %>% 
-    select(Year, AllAges, CrudeRate, EASR, WASR, StandardisedRatio, HBName, DataType, CancerSite) %>% 
-    filter(DataType == input$datatype_input_compare) %>% 
-    filter(HBName != "All Scotland Data") %>% 
-    filter(CancerSite == input$Cancer_Type_Input_compare) 
-  
-  tooltip_1 <- c(paste0("Health Board: ", Cancer_Full_Data$HBName,  "<br>", "Data Type: ", input$datatype_input_compare, "<br>",  "Measure Type: ", input$graphtype_input_compare, "<br>", "Date: ", Cancer_Full_Data$Year, "<br>", "Total (All Ages): ", Cancer_Full_Data$AllAges,  "<br>",  "Crude Rate: ", round(Cancer_Full_Data$CrudeRate,2 ),  "<br>", "EASR: ", round(Cancer_Full_Data$EASR,2 ),  "<br>", "WASR: ", round(Cancer_Full_Data$WASR,2),  "<br>", "Standardised Ratio: ", round(Cancer_Full_Data$StandardisedRatio,2)))
+    filter(GeoName != "All Scotland Data") %>% 
+    filter(CancerSite == input$Cancer_Type_Input_Stats) %>% 
+  filter(GeoName %in% input$hb_name) %>% 
+    filter(Sex == input$Cancer_Gender_Input)
   
   
-  Cancer_Full_Data <- Cancer_Full_Data %>% 
-    plot_ly(x = ~ Year,
-            y = ~ get(input$graphtype_input_compare),
-            color = ~ HBName,
-            type = 'scatter',
-            mode = 'lines',
-            text=tooltip_1, 
+  Cancer_Scatter_Data <- Cancer_Scatter_Data %>% 
+    plot_ly(x = ~ AllAges,
+            y = ~ DeathsAllAges,
+            color = ~ GeoName,
             hoverinfo="text" ) %>% 
-    layout(xaxis = list(title = "Year"),
-           yaxis = list(title = input$graphtype_input_compare))
+    layout(xaxis = list(title = "All Ages"),
+           yaxis = list(title = "All Deaths"))
   
 })
 
+output$hb_cancer_outlier_box <-  renderPlotly({
+  Cancer_Scatter_Data <-  Cancer_Scatter_Data %>% 
+    select(-CancerSiteICD10Code) %>% 
+    filter(GeoName != "All Scotland Data") %>% 
+    filter(CancerSite == input$Cancer_Type_Input_Stats) %>% 
+    filter(GeoName %in% input$hb_name) %>% 
+    filter(Sex == input$Cancer_Gender_Input) %>% 
+  plot_ly(x = ~GeoName,
+          y = ~DeathsAllAges,
+          color = ~ GeoName,
+          type = "box",
+          quartilemethod="inclusive")%>% 
+    layout(xaxis = list(title = "All Ages"),
+           yaxis = list(title = "All Deaths"))
+  
+})
