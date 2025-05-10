dfcb8d21e059f0884a4fbc98fcbd5191093d0e3f
Benny24aa <harleyb101010@gmail.com>
2025-05-10 20:53:01 +0100

    scotland data filtered

--- Open Data Dashboard/Global/Cancer/Waiting Times.R
+++ Open Data Dashboard/Global/Cancer/Waiting Times.R
@@ -1,5 +1,5 @@
 Cancer_Waiting_Times_31_days <- get_resource(res_id = "58527343-a930-4058-bf9e-3c6e5cb04010") %>% 
-  filter(HBT != "S92000003")
+  filter(HBT != "S92000003") 
 
 Cancer_Waiting_Times_31_days_T <- Cancer_Waiting_Times_31_days %>% 
   select(Quarter,HB,HBT, CancerType ,NumberOfEligibleReferrals31DayStandard, NumberOfEligibleReferralsTreatedWithin31Days) %>% 
