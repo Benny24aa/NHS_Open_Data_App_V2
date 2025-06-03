8aaabfaf4284103e895d84699339bd4e9b905ce6
Benny24aa <harleyb101010@gmail.com>
2025-06-03 18:52:52 +0100

    Minor fixes to download table and addition to cancer scatter data to health board overview download page

--- Open Data Dashboard/Global/Data Download Lists.R
+++ Open Data Dashboard/Global/Data Download Lists.R
@@ -4,6 +4,6 @@ reference_file_download_list <- c("Health Board Lookup" = "HB_Lookup",
                                   "Interminate Zone Lookup" = "Interminate_Zone_Lookup",
                                   "Data Zone Lookup" = "Data_Zone_Lookup")
 
-Cancer_Download_List <- c("All Cancer and Incidence Data" = "Cancer_Full_Data")
+Cancer_Download_List <- c("All Cancer and Incidence Data" = "Cancer_Full_Data", "Scatter Plot Data" = "Cancer_Scatter_Data")
 
 cancer_waiting_list_download_list <- c("31 Days Standard" = "Cancer_Waiting_Times_31_days_T", "62 Days Standard" = "Cancer_Waiting_Times_62_days_T")
