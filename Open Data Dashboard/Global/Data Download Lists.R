8e6e9d05dc7d1c38a288ad7a0f07f102d1e09b3d
Benny24aa <harleyb101010@gmail.com>
2025-06-03 18:38:44 +0100

    added download list

--- Open Data Dashboard/Global/Data Download Lists.R
+++ Open Data Dashboard/Global/Data Download Lists.R
@@ -5,3 +5,5 @@ reference_file_download_list <- c("Health Board Lookup" = "HB_Lookup",
                                   "Data Zone Lookup" = "Data_Zone_Lookup")
 
 Cancer_Download_List <- c("All Cancer and Incidence Data" = "Cancer_Full_Data")
+
+cancer_waiting_list_download_list <- c("31 Days Standard" = "Cancer_Waiting_Times_31_days_T", "62 Days Standard" = "Cancer_Waiting_Times_62_days_T")
