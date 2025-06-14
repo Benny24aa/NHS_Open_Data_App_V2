41237b35ec0032d5789d4e0d8aae2a69cb7814c1
Benny24aa <harleyb101010@gmail.com>
2025-06-14 13:50:46 +0100

    Minor text change for windowtitle upon deployment

--- Open Data Dashboard/ui.R
+++ Open Data Dashboard/ui.R
@@ -18,7 +18,7 @@ navbarPage(title = div(tags$a(img(src="", width=120, alt = ""),
                               href= "",
                               target = "_blank"),
                        style = "position: relative; top: -10px;"),
-           windowTitle = "Cancer Dashboard Application", #title for browser tab
+           windowTitle = "NHS Open Dashboard", #title for browser tab
            header = tags$head(includeCSS("www/styles.css"), # CSS styles
                               HTML("<html lang='en'>")),
            
