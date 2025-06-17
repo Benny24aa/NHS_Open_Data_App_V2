149f2c3588c2cdb021f8ad45096739b6b56d4c4f
Benny24aa <harleyb101010@gmail.com>
2025-06-17 20:42:50 +0100

    GDPR buttons added to feedback and report a bug pages with titles at the top of the page using  dashboard color theme

--- Open Data Dashboard/UI/feedback.R
+++ Open Data Dashboard/UI/feedback.R
@@ -1,7 +1,15 @@
 Feedback <- tabPanel(
                      title = "Feedback", 
                      icon = icon("comment"),
-         h4("We'd love your feedback!"),
+                     
+                     fluidRow(
+                       column(6,
+                              h2("Provide Feedback", style = "color:  #336699 ; font-weight: 600"))),
+                     
+                     fluidRow(
+                       column(6, actionButton("gdpr_modal", tags$b("GDPR Information"),
+                                              icon = icon('calendar-alt')))),
+        h4("We'd love your feedback!"),
          textAreaInput("feedback_text", "Your feedback:", ""),
          actionButton("submit_feedback", "Submit")
 )
