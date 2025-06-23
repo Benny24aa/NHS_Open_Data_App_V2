f50fc2ac20700dd589e9964e7e0ed9a4d4f6bca1
Benny24aa <harleyb101010@gmail.com>
2025-06-23 21:53:38 +0100

    minor fixes of text

--- Open Data Dashboard/UI/Cancer/Cancer UI Setup.R
+++ Open Data Dashboard/UI/Cancer/Cancer UI Setup.R
@@ -30,7 +30,7 @@ The cancer sites reported on include: bladder, bone and connective tissue, brain
                                               h4(tags$b("Open Source Code Information" , style = "color:  #336699 ; font-weight: 600")),
                                               p("This GitHub repository contains the complete source code for an interactive web dashboard designed to visualize cancer incidence and mortality statistics across Healthboard."),
                                               
-                                              p("If you wish to view the github for this dashboard please head to the following ", tags$a(href="https://github.com/Benny24aa/Scotland-Cancer-RShiny-Dashboard", icon("github"),
+                                              p("If you wish to view the github for this dashboard please head to the following ", tags$a(href="https://github.com/Benny24aa/NHS_Open_Data_App_V2", icon("github"),
                                                                                                                                           "", target="_blank"), ), 
                                               h4(tags$b(" Disclosure and Data Security Statement", style = "color:  #336699 ; font-weight: 600")),
                                               p("All content is available under the Open Government License V3.0, and is available on NHS Scotland Open Data except where otherwise stated. If you need any assistance with this, please visit the UK Government Website for more information regarding the Open Government License. This dashboard is not a representive of the NHS and therefore is not an official source of information.")),
