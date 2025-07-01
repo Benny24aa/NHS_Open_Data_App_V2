f84ad6a6d45254ee5adfb85aa760e4957733b0c7
Benny24aa <harleyb101010@gmail.com>
2025-07-01 21:08:44 +0100

    added changes to disclosure, security and disclaimer statement at bottom of landing pages

--- Open Data Dashboard/UI/Information UI.R
+++ Open Data Dashboard/UI/Information UI.R
@@ -17,12 +17,16 @@ information <- tabPanel(title = "Home",
                     The code will be updated regularly and commentated on throughout the development of the application. If you wish to view the github for this dashboard please head to the following ", tags$a(href="https://github.com/Benny24aa/NHS_Open_Data_App_V2", icon("github"),
                                                                                                                                                                                                                  "", target="_blank"), ), 
                   h4(tags$b("Type of Data Explored?", style = "color:  #336699 ; font-weight: 600" )),
-                  p("The dashboard will focus mainly on Healthboard, Council Areas, SIMD Deprivation Quinites and Deciles, Gender, and Age break downs across Scotland. The dashboard will have multiple sections which address different health related statitsics. 
+                  p("The dashboard will focus mainly on Healthboard, SIMD Deprivation Quinites and Deciles, Gender, and Age break downs across Scotland. The dashboard will have multiple sections which address different health related statitsics. 
                     Statistics such as and not limited to Cancelled Planned Operations, Delayed Discharges, A&E waiting times, Prescribed and Dispensed Data, and more. We aim to have a section for most of the categories that appear on the NHS opendata website.
                     An update on which sections have been added or will be added in the near future can be found at the top of the information section by clicking the 'New content and future updates' button."),
-                  h4(tags$b(" Disclosure and Data Security Statement", style = "color:  #336699 ; font-weight: 600")),
-                  p("All content is available under the Open Government License V3.0, and is available on NHS Scotland Open Data except where otherwise stated. If you need any assistance with this, please visit the UK Government Website for more information regarding the Open Government License. This dashboard is not a representive of the NHS and therefore is not an official source of information.")),
-         )#End of Fluid Row
+                  h4(tags$b(" Disclosure, Data Security Statement and Disclaimer", style = "color:  #336699 ; font-weight: 600")),
+                  p("All content is available under the Open Government License V3.0, and is available on NHS Scotland Open Data except where otherwise stated. If you need any assistance with this, please visit the UK Government Website for more information regarding the Open Government License. This dashboard is not a representive of the NHS and therefore is not an official source of information. This means that this RShiny open data application is an independent project and is not affiliated with, endorsed by, or representative of the National Health Service (NHS) or any of its associated organizations. The NHS is mentioned solely because the data used in this application originates from publicly available NHS open data sources. ")),
+         
+           
+           ),#End of Fluid Row
+         
+         
          
 ) #End of TabPanel
 
