214b8876988408b90fbd8e71f6a536dc07028ba9
Benny24aa <harleyb101010@gmail.com>
2025-07-04 16:57:48 +0100

    Code added for cleaning up dataset, and removing small number of entries that are classed as Type 3 (minor injuries) in hospitals reference files (data quality work, 20/30 rows from 2015, no big issue).

--- Open Data Dashboard/Global/A&E/Weekly A&E/Weekly A&E Data.R
+++ Open Data Dashboard/Global/A&E/Weekly A&E/Weekly A&E Data.R
@@ -1,6 +1,39 @@
+#### Calling in data from PHS website
 WeeklyAE <- get_resource(res_id = "a5f7ca94-c810-41b5-a7c9-25c18d43e5a4")
+AE_Sites <- get_resource(res_id = "1a4e3f48-3d9b-4769-80e9-3ef6d27852fe")
 
+
+#### Converting to Date Format
 WeeklyAE$WeekEndingDate <- as.Date(
   as.character(WeeklyAE$WeekEndingDate),
   format = "%Y%m%d"
 )
+
+#### Removing useless columns 
+
+WeeklyAE <- WeeklyAE %>% 
+  select(-Country, -DepartmentType)
+
+#### Creating lookup for A&E data to perform join
+
+HB_Lookup_AE <- HB_Lookup %>% 
+  select(-GeoType) %>% 
+  rename(HBT = HB)
+
+#### Cleaning A&E Sites lookup File
+
+AE_Sites <- AE_Sites %>% 
+  select(TreatmentLocationName, TreatmentLocationCode, CurrentDepartmentType)
+
+#### Joining HBT code with HB Name to dataset
+WeeklyAE <- full_join(WeeklyAE, HB_Lookup_AE, by = "HBT") %>%
+  select(-HBT) %>% 
+  rename(TreatmentLocationCode = TreatmentLocation)
+
+#### Joining HBT code with HB Name to dataset
+WeeklyAE <- left_join(WeeklyAE, AE_Sites, by = "TreatmentLocationCode") 
+
+WeeklyAE <- WeeklyAE %>% 
+  filter(CurrentDepartmentType == "Type 1") ### Removes Type 3 minor injury rows (around 20/30 exist in 2015 for two glasgow locations)
+
+
