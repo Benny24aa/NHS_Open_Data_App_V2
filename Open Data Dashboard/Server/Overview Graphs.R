c78cd32ecedf5eca9c039442c0578b3522a080a9
Benny24aa <harleyb101010@gmail.com>
2025-07-05 17:01:59 +0100

    Tooltips added for number of attendances to AE graph for both historic and current data

--- Open Data Dashboard/Server/Overview Graphs.R
+++ Open Data Dashboard/Server/Overview Graphs.R
@@ -747,6 +747,26 @@ output$total_weekly_ae_attendance_graph <- renderPlotly({
   WeeklyAE_WithRolling <- WeeklyAE_Filtered %>%
     left_join(HistoricWeeklyAvg, by = "WeekNum")
   
+  # Add tooltips
+  WeeklyAE_Filtered <- WeeklyAE_Filtered %>%
+    mutate(
+      text = paste0(
+        "Week Ending: ", format(WeekEndingDate, "%d-%b-%Y"), "<br>",
+        "Health Board: ", HBName, "<br>",
+        "Hospital: ", TreatmentLocationName, "<br>",
+        "Category: ", AttendanceCategory, "<br>",
+        "Attendances: ", NumberOfAttendancesEpisode
+      )
+    )
+  
+  WeeklyAE_WithRolling <- WeeklyAE_WithRolling %>%
+    mutate(
+      text_hist = paste0(
+        "Week Ending: ", format(WeekEndingDate, "%d-%b-%Y"), "<br>",
+        "Historic Weekly Avg: ", round(HistoricRollingAvg, 1)
+      )
+    )
+  
   # Plot
   plot_ly() %>%
     add_trace(
@@ -757,6 +777,7 @@ output$total_weekly_ae_attendance_graph <- renderPlotly({
       type = 'scatter',
       mode = 'lines',
       name = 'Current Year(s)',
+      text = ~text,
       hoverinfo = "text"
     ) %>%
     add_trace(
@@ -767,6 +788,7 @@ output$total_weekly_ae_attendance_graph <- renderPlotly({
       mode = 'lines',
       name = paste0("Historic Weekly Avg (", paste(historic_years, collapse = "-"), ")"),
       line = list(dash = "dot", color = '#006400'),  # dark green
+      text = ~text_hist,
       hoverinfo = "text"
     ) %>%
     layout(
