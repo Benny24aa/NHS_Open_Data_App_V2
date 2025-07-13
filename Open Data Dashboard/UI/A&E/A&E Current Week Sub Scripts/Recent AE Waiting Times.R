1e36cb51de7d8305d663ee4f60b2b5048a191e92
Benny24aa <harleyb101010@gmail.com>
2025-07-13 21:31:22 +0100

    structure change to make coding each conditional panel easier

---
+++ Open Data Dashboard/UI/A&E/A&E Current Week Sub Scripts/Recent AE Waiting Times.R
@@ -0,0 +1,59 @@
+AE_Recent_Waiting_Times <- conditionalPanel(
+  condition= 'input.ae_recent_select == "Recent_AE_Tab"', 
+  
+  uiOutput("currentAEHeadersummary"),
+  
+  # Styled section: filters + value boxes
+  div(
+    style = "background-color: #cce5ff; padding: 15px; border-radius: 10px; margin-top: 15px;",
+    
+    tags$style(HTML("
+  .custom-select .form-group {
+    background-color: #cce5ff !important;
+  }
+
+  .custom-select .selectize-control {
+    background-color: #cce5ff !important;
+  }
+
+  .custom-select .selectize-input {
+    background-color: #cce5ff !important;
+    color: #254a7c !important;
+    font-weight: bold;
+    border: 1px solid #999999;           /* Grey border */
+    border-radius: 4px;
+  }
+
+  .custom-select .selectize-dropdown {
+    background-color: #cce5ff !important;
+  }
+
+  .custom-select .selectize-dropdown-content .option {
+    color: #254a7c !important;
+    font-weight: bold;
+  }
+
+  .custom-select label {
+    color: #003366 !important;
+    font-weight: bold;
+    font-size: 18px;
+  }
+")),
+    
+    fluidRow(
+      column(3,div(class = "custom-select", selectInput("HBName_Current_AE", "Select Health Board", 
+                                                        choices = unique(WeeklyAE_Healthboard$HBName)))
+             
+      ),
+      column(3,
+             div(class = "custom-select",
+                 selectInput("AttendanceCategory_Current_AE", "Select Category", 
+                             choices = unique(WeeklyAE_Healthboard$AttendanceCategory)))
+      )
+    ),
+    
+    br(),
+    # AE Value Boxes
+    uiOutput("currentAEBoxes")
+  )
+)
--- Open Data Dashboard/UI/A&E/A&E Current Week.R
+++ Open Data Dashboard/UI/A&E/A&E Current Week.R
@@ -1,3 +1,5 @@
+source("UI/A&E/A&E Current Week Sub Scripts/Recent AE Waiting Times.R")
+
 CurrentAEUI <- tabPanel(
   title = "Most Recent Accident and Emergency Statistics", 
   icon = icon("car-on"),
@@ -22,63 +24,5 @@ CurrentAEUI <- tabPanel(
   br(),
   
   
-  conditionalPanel(
-    condition= 'input.ae_recent_select == "Recent_AE_Tab"', 
-  
-    uiOutput("currentAEHeadersummary"),
-  
-  # Styled section: filters + value boxes
-  div(
-    style = "background-color: #cce5ff; padding: 15px; border-radius: 10px; margin-top: 15px;",
-    
-    tags$style(HTML("
-  .custom-select .form-group {
-    background-color: #cce5ff !important;
-  }
-
-  .custom-select .selectize-control {
-    background-color: #cce5ff !important;
-  }
-
-  .custom-select .selectize-input {
-    background-color: #cce5ff !important;
-    color: #254a7c !important;
-    font-weight: bold;
-    border: 1px solid #999999;           /* Grey border */
-    border-radius: 4px;
-  }
-
-  .custom-select .selectize-dropdown {
-    background-color: #cce5ff !important;
-  }
-
-  .custom-select .selectize-dropdown-content .option {
-    color: #254a7c !important;
-    font-weight: bold;
-  }
-
-  .custom-select label {
-    color: #003366 !important;
-    font-weight: bold;
-    font-size: 18px;
-  }
-")),
-   
-    fluidRow(
-      column(3,div(class = "custom-select", selectInput("HBName_Current_AE", "Select Health Board", 
-                                                       choices = unique(WeeklyAE_Healthboard$HBName)))
-            
-      ),
-      column(3,
-             div(class = "custom-select",
-                 selectInput("AttendanceCategory_Current_AE", "Select Category", 
-                             choices = unique(WeeklyAE_Healthboard$AttendanceCategory)))
-      )
-    ),
-    
-    br(),
-    # AE Value Boxes
-    uiOutput("currentAEBoxes")
-  )
-)
+  AE_Recent_Waiting_Times
 )
