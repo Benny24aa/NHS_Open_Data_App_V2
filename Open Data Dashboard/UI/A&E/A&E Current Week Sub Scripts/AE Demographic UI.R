42e2192dbb6d752ab5cc5251e4474726d1cfebe8
Benny24aa <harleyb101010@gmail.com>
2025-07-24 20:01:35 +0100

    HB filter add for demographic tab, alongside blue background for implementation of 5 boxes for each deprivation quintile

---
+++ Open Data Dashboard/UI/A&E/A&E Current Week Sub Scripts/AE Demographic UI.R
@@ -0,0 +1,13 @@
+AE_Demographic <- conditionalPanel(
+  condition= 'input.ae_recent_select == "Recent_AE_Demographic_Tab"',
+  
+  div(
+    style = "background-color: #cce5ff; padding: 15px; border-radius: 10px; margin-top: 15px;",
+  fluidRow(
+    column(3,div(class = "custom-select", selectInput("HBName_Current_AE_Demographic", "Select Health Board", 
+                                                      choices = unique(WeeklyAE_Healthboard$HBName))))
+  
+  )  
+  ) ## end of division for summary boxes 
+  
+) # End of Conditional Panel
