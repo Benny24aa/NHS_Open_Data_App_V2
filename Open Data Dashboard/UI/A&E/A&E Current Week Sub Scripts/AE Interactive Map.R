88383104b55d3c82e1573eccf843675b9381f6af
Benny24aa <harleyb101010@gmail.com>
2025-10-12 15:10:11 +0100

    Filters added for interactive map page

--- Open Data Dashboard/UI/A&E/A&E Current Week Sub Scripts/AE Interactive Map.R
+++ Open Data Dashboard/UI/A&E/A&E Current Week Sub Scripts/AE Interactive Map.R
@@ -1,20 +1,54 @@
 AE_Interactive_Map <- tabPanel(title = "Interactive Map", 
-                       icon = icon("map")
+                       icon = icon("map"),
                        
+                     #  uiOutput("Template"),
                        
+                       # Styled section: filters + value boxes
+                       div(
+                         style = "background-color: #cce5ff; padding: 15px; border-radius: 10px; margin-top: 15px;",
+                         
+                         fluidRow(
+                           column(3,div(class = "custom-select", selectInput("HBName_Map_AE", "Select Health Board", 
+                                                                             choices = unique(WeeklyAE_Healthboard$HBName)))
+                                  
+                           ),
+                           column(3,
+                                  div(class = "custom-select",
+                                      selectInput("AttendanceCategory_Map_AE", "Select Category", 
+                                                  choices = unique(WeeklyAE_Healthboard$AttendanceCategory)))
+                           ),
+                           
+                           column(3, 
+                                  div(class = "custom-select", selectInput(
+                                    inputId = "ae_map_measure_select",
+                                    label = "Select Measure",
+                                    choices = c(
+                                      "Total Attendances" = "TotalAttendances",
+                                      "Over 4 Hours" = "TotalOver4Hours",
+                                      "Over 8 Hours" = "TotalOver8Hours",
+                                      "Over 12 Hours" = "TotalOver12Hours",
+                                      "Within 4 Hours" = "TotalWithin4Hours"
+                                    ),
+                                    selected = "TotalAttendances"
+                                  ))
+                           )
+                         ),
+                       ), ## end of division
+                       br(),
                        
+                     #  uiOutput("template"),
+                       br(),
                        
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-                       
+                       div(
+                         style = "background-color: #f0f0f0; padding: 20px; border-radius: 10px;",
+
+                         fluidRow(
+                           column(12,
+                                  #plotlyOutput("ae_recent_iso_graph", height = "600px")
+                           )
+                         )
                        )
+                       
+                       
+                       
+                       ) # End of Panel
