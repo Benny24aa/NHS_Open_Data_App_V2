e9c23e3bef9c5b9a6c1e458f7f735fc721e5aec4
Benny24aa <harleyb101010@gmail.com>
2025-10-27 17:00:23 +0000

    Tabs added for machine learning examples

---
+++ Open Data Dashboard/UI/Machine Learning/Random Forest Outliers.R
@@ -0,0 +1,6 @@
+Random_Forest_Outliers_UI <- tabPanel(title = "Random Forest - Anomonaly Detection", 
+                        icon = icon("tree")
+                        
+                        
+                        
+) #End of TabPanel
