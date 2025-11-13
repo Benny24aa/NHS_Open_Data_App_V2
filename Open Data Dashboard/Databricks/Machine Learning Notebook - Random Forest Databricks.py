8d990643025820221c71ba247fbf0bcb97745bc9
Benny24aa <harleyb101010@gmail.com>
2025-11-13 17:46:39 +0000

    Databricks - Cleaning of data and joining on GP list for analysis

--- Open Data Dashboard/Databricks/Machine Learning Notebook - Random Forest Databricks.py
+++ Open Data Dashboard/Databricks/Machine Learning Notebook - Random Forest Databricks.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 
 # Read your Lakeflow Declarative Pipelines output table
 df_spark = spark.read.table("workspace.default.presdisp_clean")
-
+df_gp = spark.read.table("nhs_waiting_times_dashboard.default.gplist")
 # Convert to Pandas for sklearn
 df = df_spark.toPandas()
+df_gp = df_gp.toPandas()
 
 df["PrescriberLocation"] = pd.to_numeric(df["PrescriberLocation"], errors="coerce")
 
@@ -21,3 +22,11 @@ df["PaidDateMonth"] = pd.to_datetime(
 )
 df["MonthNum"] = df["PaidDateMonth"].dt.month
 df["Year"] = df["PaidDateMonth"].dt.year
+
+df_gp = df_gp[["PrescriberLocation", "HB", "HSCP", "DataZone", "GPCluster", "PracticeListSize"]] \
+             .drop_duplicates() 
+
+df = df.query('PrescriberLocationType == "GP PRACTICE" and DispenserLocationType == "COMMUNITY PHARMACY"')
+df = df.merge(df_gp, on="PrescriberLocation", how="left")
+
+display(df)
