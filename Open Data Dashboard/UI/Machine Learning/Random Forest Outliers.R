85706ff002e29a998718469ec77ec5ea50a968c2
Benny24aa <harleyb101010@gmail.com>
2025-12-14 10:52:57 +0000

    value added to allow for initialisation of databricks based on conditions of observeevent

--- Open Data Dashboard/UI/Machine Learning/Random Forest Outliers.R
+++ Open Data Dashboard/UI/Machine Learning/Random Forest Outliers.R
@@ -1,6 +1,11 @@
-Random_Forest_Outliers_UI <- tabPanel(title = "Random Forest - Anomonaly Detection", 
-                        icon = icon("tree")
-                        
+
+Random_Forest_Outliers_UI <- tabPanel(
+  title = "Random Forest - Anomaly Detection",
+  icon  = icon("tree"),
+  value = "ml_random_anomaly",
+  
+  actionButton("run_anomaly", "Run anomaly detection"),
+  tableOutput("anomaly_table")
                         
                         
 ) #End of TabPanel
