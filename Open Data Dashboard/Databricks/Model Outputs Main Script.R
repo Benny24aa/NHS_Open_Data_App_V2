12042f30e70b7954586288fe9c71c127588d19ad
Benny24aa <harleyb101010@gmail.com>
2026-01-25 17:16:20 +0000

    Creating a setup script for models

--- Open Data Dashboard/Databricks/Model Outputs Main Script.R
+++ Open Data Dashboard/Databricks/Model Outputs Main Script.R
@@ -1,3 +1,6 @@
+#### Script Setup ###
+
+
 # --- Load libraries ---
 library(data.table)
 library(phsopendata)   # For live PHS data
@@ -7,6 +10,31 @@ library(ranger)        # Much faster alternative to randomForest
 library(arrow)
 library(glue)
 
+
 ###### Define the type of model you wish to run, this could be Alpha, Beta, Charlie, or Delta.
 
 model_type <- "Alpha"
+
+# "none"	No variable importance computed (fastest).
+# "impurity"	Gini impurity decrease (classification) or variance decrease (regression). Fast, but biased toward variables with many categories.
+# "impurity_corrected"	Bias-corrected impurity importance. Slower but more reliable than plain impurity.
+# "permutation"	Permutation importance. Measures drop in prediction accuracy when a variable is permuted. Reliable but slower.
+
+importance_type <- "impurity_corrected"
+
+# We look at 5, 10, 20, 50 and 100 for the number of trees
+
+tree_number <- 100
+
+####### Preparing Data for Random Forest Model of Choice
+
+# --- Load main prescribing dataset ---
+df <- read_parquet("Databricks/presdisp.parquet")
+# Convert PrescriberLocation to numeric
+df$PrescriberLocation <- as.numeric(df$PrescriberLocation)
+
+# --- Load GP practice list and join ---
+gp_list <- get_resource(res_id = "30b06220-17ad-44e8-b6c5-658d41ec1ea5") %>%
+  select(PracticeCode, HB, HSCP, DataZone, GPCluster, PracticeListSize) %>%
+  distinct() %>%
+  rename(PrescriberLocation = PracticeCode) 
