ae3885aa074b1fb2a164ea605c0f08964a8274ff
Benny24aa <harleyb101010@gmail.com>
2026-01-25 17:46:16 +0000

    Building in the cleaning process for each model, fixed mistake with beta model as it brings in more from GP lookup such as postcode and address which other 3 models don't consider

--- Open Data Dashboard/Databricks/Model Outputs Main Script.R
+++ Open Data Dashboard/Databricks/Model Outputs Main Script.R
@@ -13,7 +13,11 @@ library(glue)
 
 ###### Define the type of model you wish to run, this could be Alpha, Beta, Charlie, or Delta.
 
-model_type <- "Alpha"
+model_type <- "Beta"
+
+##### Yes or No to this question - If you have ran the data cleaning process before, press Yes so you don't have to go through it again for the training data
+
+train_data_exist <- "No" ## Note some models have slightly different cleaning processes, so only say yes if you want to run the same model twice with other hyper parameters 
 
 # "none"	No variable importance computed (fastest).
 # "impurity"	Gini impurity decrease (classification) or variance decrease (regression). Fast, but biased toward variables with many categories.
@@ -26,6 +30,10 @@ importance_type <- "impurity_corrected"
 
 tree_number <- 100
 
+
+
+if (train_data_exist == "No") {
+
 ####### Preparing Data for Random Forest Model of Choice
 
 # --- Load main prescribing dataset ---
@@ -33,11 +41,20 @@ df <- read_parquet("Databricks/presdisp.parquet")
 # Convert PrescriberLocation to numeric
 df$PrescriberLocation <- as.numeric(df$PrescriberLocation)
 
+if (model_type != "Beta") {
 # --- Load GP practice list and join ---
 gp_list <- get_resource(res_id = "30b06220-17ad-44e8-b6c5-658d41ec1ea5") %>%
   select(PracticeCode, HB, HSCP, DataZone, GPCluster, PracticeListSize) %>%
   distinct() %>%
-  rename(PrescriberLocation = PracticeCode) 
+  rename(PrescriberLocation = PracticeCode) }
+
+if (model_type == "Beta") {
+  # --- Load GP practice list ---
+  gp_list <- get_resource(res_id = "30b06220-17ad-44e8-b6c5-658d41ec1ea5") %>%
+    select(PracticeCode, HB, HSCP, DataZone, GPCluster, PracticeListSize, GPPracticeName, AddressLine1, AddressLine2, AddressLine3, Postcode, PracticeType) %>%
+    distinct() %>%
+    rename(PrescriberLocation = PracticeCode)
+}
 
 # --- Load GP age profile ---
 gp_further_info <- get_resource(res_id = "ac5a7a66-7bf9-4ea0-b076-a0de9fb71ad6") %>%
@@ -67,3 +84,59 @@ gp_further_info <- gp_further_info %>%
   rename(PrescriberLocation = PracticeCode)
 
 gp_list <- left_join(gp_list, gp_further_info, by = "PrescriberLocation")
+
+# --- Filter GP prescribing ---
+df <- df %>%
+  filter(
+    PrescriberLocationType == "GP PRACTICE",
+    DispenserLocationType == "COMMUNITY PHARMACY"
+  ) %>%
+  left_join(gp_list, by = "PrescriberLocation") 
+
+if (model_type == "Alpha") {
+   df <- df %>%
+    select(PaidDateMonth, PrescriberLocation, PrescriberLocationType,
+           DispenserLocation, DispenserLocationType, NumberOfPaidItems,
+           HB, HSCP, DataZone, GPCluster, PracticeListSize, age_0_19, age_20_29, age_30_65, age_65_plus)
+}
+
+if (model_type == "Beta") {
+  df <- df %>%
+  select(
+    PaidDateMonth, PrescriberLocation, DispenserLocation, NumberOfPaidItems,
+    HB, HSCP, GPCluster, PracticeListSize,
+    age_0_19, age_20_29, age_30_65, age_65_plus, GPPracticeName, AddressLine1, AddressLine2, AddressLine3, Postcode, PracticeType, PrescriberType
+  )
+}
+
+if (model_type == "Charlie") {
+  df <- df %>%
+    select(PaidDateMonth, PrescriberLocation, PrescriberLocationType,
+           DispenserLocation, DispenserLocationType, NumberOfPaidItems,
+           HB, HSCP, DataZone, GPCluster, PracticeListSize, age_0_19, age_20_29, age_30_65, age_65_plus, PrescriberType)
+}
+
+if (model_type == "Delta") {
+  df <- df %>%
+    select(PaidDateMonth, PrescriberLocation, PrescriberLocationType,
+           DispenserLocation, DispenserLocationType, NumberOfPaidItems,
+           HB, HSCP, DataZone, GPCluster, PracticeListSize, age_0_19, age_20_29, age_30_65, age_65_plus, PrescriberType)
+  
+}
+
+df <- df %>%
+  mutate(
+    PaidDateMonth = lubridate::ym(PaidDateMonth),
+    MonthNum = lubridate::month(PaidDateMonth),
+    Year = lubridate::year(PaidDateMonth)
+  )
+
+if (model_type == "Beta") {
+  df <- df %>%
+    mutate(log_items = log1p(NumberOfPaidItems))
+}
+
+
+
+
+}
