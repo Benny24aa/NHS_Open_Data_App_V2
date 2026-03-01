8a9f87e7731768eb900b6a3b14ea5afdcf3be587
Benny24aa <harleyb101010@gmail.com>
2026-03-01 19:38:18 +0000

    Provisional respiratory disease averages for forecasting

--- Open Data Dashboard/Databricks/A&E/respiratory_diseases_pipeline.R
+++ Open Data Dashboard/Databricks/A&E/respiratory_diseases_pipeline.R
@@ -24,15 +24,55 @@ Respiratory_Diseases <- get_resource(res_id = "212412ba-cff2-43b9-bd40-f8d80688d
 
 Respiratory_Diseases <- Respiratory_Diseases %>%
   mutate(
-    WeekEndingDate = lubridate::ymd(WeekEndingDate)
+    WeekEndingDate = lubridate::ymd(WeekEndingDate),
+    WeekNum = lubridate::week(WeekEndingDate),
+    Year = lubridate::year(WeekEndingDate)
   )
 
 setDT(Respiratory_Diseases) 
 
-wide_df <- dcast(
+resp_month_avg <- Respiratory_Diseases %>%
+  mutate(MonthNum = month(WeekEndingDate)) %>%
+  filter(Year > "2022") %>% 
+  select(-Year) %>% 
+  group_by(WeekNum, HBT, Pathogen) %>%
+  summarise(across(starts_with("NumberCasesPerWeek"),
+                   mean,
+                   na.rm = TRUE)) %>% 
+  ungroup()
+
+resp_week_avg_wide <- resp_month_avg %>%
+  pivot_wider(
+    names_from  = Pathogen,
+    values_from = starts_with("NumberCasesPerWeek"),
+    names_prefix = "NumberCasesPerWeek_"
+  )
+
+resp_month_avg_rates <- Respiratory_Diseases %>%
+  mutate(MonthNum = month(WeekEndingDate)) %>%
+  filter(Year > "2022") %>% 
+  select(-Year) %>% 
+  group_by(WeekNum, HBT, Pathogen) %>%
+  summarise(across(starts_with("RateCasesPerWeek"),
+                   mean,
+                   na.rm = TRUE)) %>% 
+  ungroup()
+
+resp_week_avg_wide_rates <- resp_month_avg_rates %>%
+  pivot_wider(
+    names_from  = Pathogen,
+    values_from = starts_with("RateCasesPerWeek"),
+    names_prefix = "RateCasesPerWeek_"
+  )
+
+
+
+Respiratory_Diseases <- dcast(
   Respiratory_Diseases,
   WeekEndingDate + HBT ~ Pathogen,
   value.var = c("NumberCasesPerWeek", "RateCasesPerWeek")
 )
 
-wide_df[is.na(wide_df)] <- 0 ### This is because covid didn't exist pre 2020
+Respiratory_Diseases[is.na(Respiratory_Diseases)] <- 0 ### This is because covid didn't exist pre 2020
+
+############## NOTE TO SELF, these are being created to replace the distinct value which will just repeat itself after the actual data runs out EG, 2026-02-22 data is the last, then the rest just copies this
